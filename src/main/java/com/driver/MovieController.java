70cfae90e38eab6e4de7f26640d3ae113b9d055b
Aman Gour <amangour562@gmail.com>
2023-01-28 02:20:47 -0800

    first soln

---
+++ src/main/java/com/driver/MovieController.java
@@ -0,0 +1,78 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @PostMapping("/add-movie")
+    public ResponseEntity addMovie(@RequestBody Movie movies) {
+        String response = movieService.addMovie(movies);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/add-director")
+    public ResponseEntity addDirector(@RequestBody Director director) {
+        String response = movieService.addDirector(director);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/add-movie-director-pair")
+    public ResponseEntity addMovieDirectorPair(@RequestParam("movieName") String movieName, @RequestParam("directorName") String directorName) {
+        String response = movieService.addMovieDirectorPair(movieName, directorName);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-movie-by-name/{name}")
+    public ResponseEntity getMovieByName(@PathVariable("name") String movieName) {
+
+        Movie movie = movieService.getMovieByName(movieName);
+        return new ResponseEntity<>(movie, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get-director-by-name/{name}")
+    public ResponseEntity getDirectorByName(@PathVariable("name") String directorName) {
+        Director director = movieService.getDirectorByName(directorName);
+        return new ResponseEntity<>(director, HttpStatus.FOUND);
+    }
+
+    @GetMapping("get-movies-by-director-name/{director}")
+    public ResponseEntity getMoviesByDirectorName(@PathVariable("director") String directorName) {
+
+        return new ResponseEntity<>(movieService.getMoviesByDirectorName(directorName), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get-all-movies")
+    public ResponseEntity findAllMovies() {
+
+        return new ResponseEntity<>(movieService.findAllMovies(), HttpStatus.FOUND);
+    }
+
+
+
+    @DeleteMapping("/delete-director-by-name")
+    public ResponseEntity deleteDirectorByName(@RequestParam("name") String directorName) {
+
+        String response=movieService.deleteDirectorByName(directorName);
+        return new ResponseEntity<>(response,HttpStatus.FOUND);
+
+    }
+
+    @DeleteMapping("/delete-all-directors")
+    public ResponseEntity deleteAllDirectors(){
+        String response=movieService.deleteAllDirectors();
+        return new ResponseEntity(response,HttpStatus.FOUND);
+
+    }
+
+}
---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,118 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MovieRepository {
+
+    // name of movies , MoviesClass
+
+    Map<String, Movie> moviesDatabase = new HashMap<>();
+
+    //name of director,Director obj
+    Map<String, Director> directorDatabase = new HashMap<>();
+
+    // this data base for storing the pairs of movie and direct
+    //name of director    movies
+    // example   Aman -> [kick,B,C,D]
+    Map<String, List<String>> pairdirectMovesDB = new HashMap<>();
+
+
+    public String addMovie(Movie movies) {
+        String name = movies.getName();
+        moviesDatabase.put(name, movies);
+        return "Movie Added Sucessfully";
+    }
+
+    public String addDirector(Director director) {
+        String name = director.getName();
+        directorDatabase.put(name, director);
+        return "Director Added Sucessfully";
+    }
+
+    public String addMovieDirectorPair(String movieName, String directorName) {
+        if (moviesDatabase.containsKey(movieName) && directorDatabase.containsKey(directorName)) {
+
+            List<String> movies = new ArrayList<>();
+            if (pairdirectMovesDB.containsKey(directorName)) {
+                movies = pairdirectMovesDB.get(directorName);
+            }
+            movies.add(movieName);
+            pairdirectMovesDB.put(directorName, movies);
+        }
+        return "Pair Added Sucessfully";
+
+    }
+
+    public Movie getMovieByName(String movieName) {
+        return moviesDatabase.get(movieName);
+    }
+
+    public Director getDirectorByName(String directorName){
+        return directorDatabase.get(directorName);
+    }
+    public List<String> getMoviesByDirectorName(String directorName){
+        List<String>movies=new ArrayList<>();
+        if(pairdirectMovesDB.containsKey(directorName))movies= pairdirectMovesDB.get(directorName);
+        return movies;
+
+    }
+    public List<String>  findAllMovies(){
+        List<String>allMovies=new ArrayList<>();
+
+        for(String movieName:moviesDatabase.keySet()){
+            allMovies.add(movieName);
+
+        }
+        return allMovies;
+    }
+
+    public String deleteDirectorByName(String directorName){
+
+        List<String>movies=new ArrayList<>();
+
+        if(pairdirectMovesDB.containsKey(directorName)){
+            movies=pairdirectMovesDB.get(directorName);
+
+            for(String movie:movies){
+                if(moviesDatabase.containsKey(movie)){
+                    moviesDatabase.remove(movie);
+                }
+            }
+            pairdirectMovesDB.remove(directorName);
+        }
+        if(directorDatabase.containsKey(directorName))directorDatabase.remove(directorName);
+
+        return "Delete Sucessfully";
+    }
+
+    public String deleteAllDirectors(){
+        List<String>movies=new ArrayList<>();
+        for(String director:pairdirectMovesDB.keySet()){
+           movies=pairdirectMovesDB.get(director);
+
+           for(String movieName:movies){
+               if(moviesDatabase.containsKey(movieName))moviesDatabase.remove(movieName);
+           }
+        }
+
+        for(String name: pairdirectMovesDB.keySet()){
+            if(directorDatabase.containsKey(name))directorDatabase.remove(name);
+
+            pairdirectMovesDB.remove(name);
+        }
+        return "delet Sucessfully";
+
+    }
+
+
+
+
+
+
+}
